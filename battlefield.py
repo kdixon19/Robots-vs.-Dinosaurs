160bdf7cab84a286a22205ac457145403600d3d0
IN THE DOOR 2 <kristoffdixon19@gmail.com>
2022-10-16 12:06:22 -0400

    Added the bonus portion, where Robot has a choice between three weapons before each attack

--- battlefield.py
+++ battlefield.py
@@ -1,6 +1,7 @@
 import random
 from robot import Robot
 from dinosaur import Dinosaur
+from weapon import Weapon
 
 class Battlefield:
 
@@ -20,11 +21,15 @@ class Battlefield:
     def battle_phase(self, attacker_1, attacker_2):
         battle = True
         while battle == True:
+            if attacker_1 == self.robot:
+                self.weapon_choice(attacker_1)
             attacker_1.attack(attacker_2)
             print(f'{attacker_2.name} just took a hit, his health is now at {attacker_2.health}')
             battle = self.determine_winner(attacker_1,attacker_2)
             if battle == False:
                 break
+            if attacker_2 == self.robot:
+                self.weapon_choice(attacker_2)
             attacker_2.attack(attacker_1)
             print(f'Oooh thats gotta hurt, {attacker_1.name} health is now reduced to {attacker_1.health}')
             battle = self.determine_winner(attacker_1, attacker_2)
@@ -56,4 +61,15 @@ class Battlefield:
     def display_winner(self, winner):
         print(f'AND OUR WINNER IS {winner.name}')
 
-    
+    def weapon_choice(self, attacker):
+        user_input = input(f"{attacker.name} weapon would you like to use? [Duskfang, DawnBreaker, Rutheful Axe]: ")
+        if user_input == "Duskfang":
+            active_weapon = Weapon('Duskfang', 100)
+            print(f'{attacker.name} has chosen Duskfang which does a mighty 100 damage!')
+        elif user_input == "DawnBreaker":
+            active_weapon = Weapon('DawnBreaker', 150)
+            print(f'{attacker.name} has chosen DawnBreaker and its holy light does an insane 150 damage!')            
+        elif user_input == "Rutheful Axe":
+            active_weapon = Weapon('Rutheful Axe', 200)
+            print(f'{attacker.name} has chosen the powerful Rutheful Axe! It injures its enemy greatly with 200 damage!')            
+        return active_weapon
--- robot.py
+++ robot.py
@@ -4,9 +4,11 @@ class Robot:
     def __init__(self, name):
         self.name = name
         self.health = 500
-        self.active_weapon = Weapon('Crusader', 50)
+        self.active_weapon = Weapon('Default', 50)
     
     def attack(self, dinosaur):
         dinosaur.health -= self.active_weapon.attack_power
+    
+
 
 
